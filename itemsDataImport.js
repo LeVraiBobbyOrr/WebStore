821786974614b9a71564192d36eb162dc8516e01
Antoine <antoinefortin351@gmail.com>
2020-05-12 20:34:09 -0400

    Create itemsDataImport.js

---
+++ itemsDataImport.js
@@ -0,0 +1,27 @@
+
+{
+	name : 'item1',
+	price : '9.99',
+	amount : '100',
+	description : 'Blah  Blah Blah  Blah Blah  Blah Blah  Blah',
+	imageUrl: '/item1.jpg'
+	
+}
+
+{
+	name : 'item2',
+	price : '19.99',
+	amount : '100',
+		description : 'Blah  Blah   Blah Blah  Blah Blah  Blah',
+
+	imageUrl: '/item1.jpg'
+}
+
+{
+	name : 'item3',
+	price : '29.99',
+	amount : '100',
+	description : 'Blah   Blah Blah  Blah Blah  Blah',
+	imageUrl: '/item1.jpg'
+}
+
